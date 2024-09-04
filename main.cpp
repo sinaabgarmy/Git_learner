d6c642d88100394a3afc8b2b1c5ab02d3a97bae8
sinaabgarmy <hades01blade02@gmail.com>
2024-09-04 17:10:16 -0600

    if stement added

--- main.cpp
+++ main.cpp
@@ -3,8 +3,12 @@ using namespace std;
 
 int main(){
 
+<<<<<<< HEAD
   //This is a comment i put here jus to see what happens
 
+=======
+//i added a comment here
+>>>>>>> 9314e84 (i added a comment here)
 int a =10;
 
 for(int i;i<10;i++){
